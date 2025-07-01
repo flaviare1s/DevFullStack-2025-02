f8f65636f5e11fa65a166d7fba1dadf6873fe54b
flaviare1s <flaviarster@gmail.com>
2025-07-01 08:20:44 -0300

    Adiciona funcionalidade "Marcar como comprado - Salvar esse Estado no localStorage "

--- 2-Front-End-Basics/30-06-flavia/index.js
+++ 2-Front-End-Basics/30-06-flavia/index.js
@@ -1,10 +1,10 @@
-const form = document.getElementById('form');
-const list = document.getElementById('list');
-const inputItem = document.getElementById("inputItem");
-const addBtn = document.getElementById('addBtn');
-const resetBtn = document.getElementById('resetBtn');
+const form = document.getElementById('form')
+const list = document.getElementById('list')
+const inputItem = document.getElementById("inputItem")
+const addBtn = document.getElementById('addBtn')
+const resetBtn = document.getElementById('resetBtn')
 
-let items = []
+let items = JSON.parse(localStorage.getItem('listaCompras')) || []
 
 function salvarDados() {
   localStorage.setItem('listaCompras', JSON.stringify(items))
@@ -13,13 +13,27 @@ function salvarDados() {
 function renderizarLista() {
   list.innerHTML = ''
   items.forEach((item, index) => {
-    const li = document.createElement('li')
-    const span = document.createElement('span')
+    const div = document.createElement('div')
+    div.classList.add('div')
 
-    span.textContent = item
+    const li = document.createElement('li')
+    li.classList.add('item')
+
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.checked = item.comprado
+
+    checkbox.addEventListener("change", () => {
+      items[index].comprado = checkbox.checked
+      salvarDados()
+      renderizarLista()
+    })
+
+    const label = document.createElement('label')
+    label.textContent = item.item
 
     const btnDelete = document.createElement('button')
-    btnDelete.textContent = 'X'
+    btnDelete.textContent = '🗑️'
     btnDelete.title = 'Remover item'
     btnDelete.classList.add('delete')
 
@@ -27,11 +41,13 @@ function renderizarLista() {
       removerItem(index)
     })
 
-    li.appendChild(span)
+    li.appendChild(div)
+    div.appendChild(checkbox)
+    div.appendChild(label)
     li.appendChild(btnDelete)
     list.appendChild(li)
 
-    list.style.display = items.length ? "flex" : "none";
+    list.style.display = items.length ? "flex" : "none"
   })
 }
 
@@ -41,7 +57,10 @@ form.addEventListener('submit', (event) => {
 
   if(!novoItem) return
 
-  items.push(novoItem)
+  items.push({
+    item: novoItem,
+    comprado: false
+  })
   salvarDados()
   renderizarLista()
 
@@ -63,3 +82,10 @@ resetBtn.addEventListener('click', () => {
     list.style.display = items.length ? "flex" : "none"
   }
 })
+
+
+// Funcionalidades:
+
+// Contador de Itens - Mostrar quantos itens tem na lista, atualizando em tempo real
+// Adicione filtros para itens 'comprados' e 'pedentes'
+// Permita ordenar alfabeticamente ou por status
